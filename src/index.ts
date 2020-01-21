178f5d7cc480d4c3c41af2f5dfb0d9f494964403
xuge <>
2020-01-21 12:35:34 +0800

    add: random tool

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import verify from './utils/verify';
 import test from './utils/test';
+import random from './utils/random';
 
-export { verify, test };
+export { verify, test, random };
---
+++ src/utils/random.ts
@@ -0,0 +1,29 @@
+export default {
+    /**
+     * 返回指定范围随机整数
+     * @param {number} min
+     * @param {number} max
+     * @returns {Number} 随机数 between[min,max]
+     */
+    integer: (min, max): number => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    /**
+     * 随机洗牌数组内容
+     * @param {Array} originArr 需要随机洗牌的数组
+     * @returns {Array} 返回随机洗牌后的新数组
+     */
+    shuffle: (originArr: []): Array<number | string> => {
+        //let arr = [...originArr];
+        const arr = originArr.slice(0);
+        for (let index = arr.length - 1; index >= 0; index--) {
+            const randomIndex = Math.floor(Math.random() * (index + 1));
+            let lastElement = arr[index];
+            let randomElement = arr[randomIndex];
+
+            [lastElement, randomElement] = [randomElement, lastElement];
+        }
+        return arr;
+    }
+};

7c50a1c296d0ed279d93b352a9b6707c00a620da
xuge <>
2020-03-18 17:49:14 +0800

    添加timeDifference && 书写声明文件

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,20 @@ import random from './utils/random';
 import objectHelper from './utils/objectHelper';
 import image from './utils/image';
 
-export { verify, test, random, objectHelper, image };
+/**
+ * 计算倒计时剩余时间⏰
+ * @param startTimestamp 开始时间戳
+ * @param endTimestamp 结束时间戳
+ */
+function timeDifference(startTimestamp: number, endTimestamp: number): { days: number; hours: number; minutes: number; seconds: number } {
+    const distanceTimestamp = startTimestamp - endTimestamp;
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distanceTimestamp / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distanceTimestamp % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distanceTimestamp % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distanceTimestamp % (1000 * 60)) / 1000);
+    //console.log(days, startTimestamp, endTimestamp);
+    return distanceTimestamp < 0 ? { days: 0, hours: 0, minutes: 0, seconds: 0 } : { days, hours, minutes, seconds };
+}
+
+export { verify, test, random, objectHelper, image, timeDifference };
